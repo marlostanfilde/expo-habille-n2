77fd3974289e353a02a34d01d62782867d4faeeb
marlostanfilde <malik.kurbanov315@gmail.com>
2026-03-09 02:24:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const menuToggle = document.getElementById("menuToggle");
 const mobileMenu = document.getElementById("mobileMenu");
+const searchToggle = document.getElementById("searchToggle");
+const searchBar = document.getElementById("searchBar");
 
 menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
   menuToggle.classList.toggle("active");
+});
+
+searchToggle.addEventListener("click", () => {
+  searchBar.classList.toggle("active");
 });
